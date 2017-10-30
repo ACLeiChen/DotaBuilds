61bd37efd72164cab4feae9311191ec9a7ec10b4
RUIJUN\ruijun <ruijunzhang@outlook.com>
2017-10-30 22:27:30 +0100

    hero amd item class impl + added heros and items json file in assets folder

--- app/src/main/java/com/dotabuilds/Data/Item.java
+++ app/src/main/java/com/dotabuilds/Data/Item.java
@@ -1,8 +1,29 @@
 package com.dotabuilds.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by Lei Chen on 2017/10/25.
  */
 
-public class Item {
+public class Item implements Serializable {
+    private Integer id;
+    private String name;
+    private final static long serialVersionUID = -6630966444884339024L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
