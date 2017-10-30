1fcacbbf01ea6d24238f904ef54c87667714de99
ACLeiChen <c1520169286@gmail.com>
2017-10-30 22:31:26 +0100

    implement loadMatches()

--- app/src/main/java/com/dotabuilds/MatchHistory/MatchHistoryPresenter.java
+++ app/src/main/java/com/dotabuilds/MatchHistory/MatchHistoryPresenter.java
@@ -5,18 +5,30 @@ import android.support.annotation.NonNull;
 import com.dotabuilds.Data.Match;
 import com.dotabuilds.Data.MatchRepository;
 
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 /**
  * Created by Lei Chen on 2017/10/26.
  */
 
 public class MatchHistoryPresenter implements MatchHistoryContract.UserActionsListener {
 
-    public MatchHistoryPresenter(MatchHistoryContract.View mMatchHistoryView, MatchRepository mMatchRepository) {
+    private final MatchRepository mMatchRepository;
+
+    private final MatchHistoryContract.View mMatchHistoryView;
+
+    private List<Match> matches;
+
+    public MatchHistoryPresenter(MatchHistoryContract.View matchHistoryView, MatchRepository matchRepository) {
+        mMatchHistoryView = checkNotNull(matchHistoryView, "matchHistory cannot be null!");
+        mMatchRepository = checkNotNull(matchRepository, "matchRepository cannot be null!");
     }
 
     @Override
     public void loadMatches(boolean forceUpdate) {
-
+        matches = mMatchRepository.getMatches();
+        mMatchHistoryView.showMatches(matches);
     }
 
     @Override
