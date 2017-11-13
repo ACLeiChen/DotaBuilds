a1eb78252ce47e6178eb2206ea16bd3bcea4a9c0
ACLeiChen <c1520169286@gmail.com>
2017-11-13 23:10:43 +0100

    add MatchHistoryAdapter

--- app/src/main/java/com/dotabuilds/MatchHistory/MatchHistoryFragment.java
+++ app/src/main/java/com/dotabuilds/MatchHistory/MatchHistoryFragment.java
@@ -1,16 +1,24 @@
 package com.dotabuilds.MatchHistory;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.dotabuilds.Data.Match;
 import com.dotabuilds.Data.MatchRepositoryImpl;
 import com.dotabuilds.R;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -23,6 +31,8 @@ public class MatchHistoryFragment extends Fragment implements MatchHistoryContra
 
     private MatchHistoryContract.UserActionsListener mUserActionsListener;
 
+    private MatchHistoryAdapter mListAdapter;
+
     public static MatchHistoryFragment newInstance(){
         return new MatchHistoryFragment();
     }
@@ -30,7 +40,7 @@ public class MatchHistoryFragment extends Fragment implements MatchHistoryContra
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        mListAdapter = new NotesAdapter(new ArrayList<Note>(0), mItemListener);
+        mListAdapter = new MatchHistoryAdapter(this.getContext(), R.layout.item_matchhistory);
         mUserActionsListener = new MatchHistoryPresenter(this, new MatchRepositoryImpl());
     }
 
@@ -52,6 +62,8 @@ public class MatchHistoryFragment extends Fragment implements MatchHistoryContra
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_matchhistory, container, false);
+        ListView listView = (ListView) root.findViewById(R.id.match_list);
+        listView.setAdapter(mListAdapter);
 
         // Pull-to-refresh
         SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.refresh_layout);
@@ -70,6 +82,48 @@ public class MatchHistoryFragment extends Fragment implements MatchHistoryContra
 
     @Override
     public void showMatchDetailUi(String matchId) {
+
+    }
+
+    private class MatchHistoryAdapter extends ArrayAdapter<Match> {
+
+        private final Context context;
+        private List<Match> matches;
+
+        private class ViewHolder {
+            public ImageView heroIcon;
+            public TextView isWon;
+            public TextView KDA;
+        }
+
+        public MatchHistoryAdapter(@NonNull Context context, int resource) {
+            super(context, resource);
+            this.context = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+            // reuse views
+            if (rowView == null) {
+                LayoutInflater inflater = context.getLayoutInflater();
+                rowView = inflater.inflate(R.layout.item_matchhistory, null);
+                // configure view holder
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.heroIcon = (ImageView) rowView
+                        .findViewById(R.id.heroIcon);
+                viewHolder.isWon = (TextView) rowView.findViewById(R.id.isWon);
+                viewHolder.KDA = (TextView) rowView.findViewById(R.id.KDA);
+
+                rowView.setTag(viewHolder);
+            }
+
+            // fill data
+            ViewHolder holder = (ViewHolder) rowView.getTag();
+
+
+            return rowView;
+        }
 
     }
 }
